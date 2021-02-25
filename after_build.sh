0cb3fd5dadc3ceb25c7387c9d72251553585abb1
limi00 <75186809+limi00@users.noreply.github.com>
2021-02-25 13:10:11 +0800

    Update after_build.sh

--- after_build.sh
+++ after_build.sh
@@ -2,4 +2,14 @@
 cd /home/runner/immortalwrt
 
 # Enter your commands here, e.g.
+
+# git clone https://github.com/immortalwrt/openwrt-passwall.git package/diy/luci-app-passwall
+# git clone https://github.com/xiaorouji/openwrt-passwall.git package/diy/luci-app-passwall
+git clone https://github.com/tty228/luci-app-serverchan.git package/diy/luci-app-serverchan
+svn co https://github.com/limi00/sirpdboy-package/trunk/luci-app-ddnsto package/diy/luci-app-ddnsto
+svn co https://github.com/limi00/sirpdboy-package/trunk/luci-app-autotimeset package/diy/luci-app-autotimeset
+svn co https://github.com/limi00/sirpdboy-package/trunk/luci-app-control-weburl package/diy/luci-app-control-weburl
+svn co https://github.com/linkease/ddnsto-openwrt/trunk/ddnsto package/network/services/ddnsto
+svn co https://github.com/linkease/ddnsto-openwrt/tree/main/luci-app-ddnsto package/diy/luci-app-ddnsto
+
 # echo "Success!"
