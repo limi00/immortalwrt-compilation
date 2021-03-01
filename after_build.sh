6b7c00b1723bc5c94f4b0c88a6ba5bdde3622b90
limi00 <75186809+limi00@users.noreply.github.com>
2021-03-01 18:54:50 +0800

    Update after_build.sh

--- after_build.sh
+++ after_build.sh
@@ -7,9 +7,9 @@ cd /home/runner/immortalwrt
 # git clone https://github.com/xiaorouji/openwrt-passwall.git package/diy/luci-app-passwall
 # git clone https://github.com/tty228/luci-app-serverchan.git package/diy/luci-app-serverchan
 # svn co https://github.com/limi00/sirpdboy-package/trunk/luci-app-ddnsto package/diy/luci-app-ddnsto
-svn co https://github.com/limi00/sirpdboy-package/trunk/luci-app-autotimeset package/diy/luci-app-autotimeset
-svn co https://github.com/limi00/sirpdboy-package/trunk/luci-app-control-weburl package/diy/luci-app-control-weburl
-svn co https://github.com/linkease/ddnsto-openwrt/trunk/ddnsto package/network/services/ddnsto
-svn co https://github.com/linkease/ddnsto-openwrt/trunk/luci-app-ddnsto package/ctcgfw/luci-app-ddnsto
+# svn co https://github.com/limi00/sirpdboy-package/trunk/luci-app-autotimeset package/diy/luci-app-autotimeset
+# svn co https://github.com/limi00/sirpdboy-package/trunk/luci-app-control-weburl package/diy/luci-app-control-weburl
+# svn co https://github.com/linkease/ddnsto-openwrt/trunk/ddnsto package/network/services/ddnsto
+# svn co https://github.com/linkease/ddnsto-openwrt/trunk/luci-app-ddnsto package/ctcgfw/luci-app-ddnsto
 
 # echo "Success!"
