696063c1232cf043a4c5674240f6cb85db053e48
limi00 <75186809+limi00@users.noreply.github.com>
2022-05-29 06:44:23 +0800

    Update clone_source.sh

--- clone_source.sh
+++ clone_source.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-git clone "https://github.com/immortalwrt/immortalwrt.git" --branch "openwrt-18.06" --single-branch "immortalwrt"
-pushd "immortalwrt"; ./scripts/feeds update -a; ./scripts/feeds install -a; popd
+git clone -b openwrt-21.02 --single-branch https://github.com/immortalwrt/immortalwrt && cd immortalwrt
+./scripts/feeds update -a && ./scripts/feeds install -a
+
+# git clone "https://github.com/immortalwrt/immortalwrt.git" --branch "openwrt-18.06" --single-branch "immortalwrt"
+# pushd "immortalwrt"; ./scripts/feeds update -a; ./scripts/feeds install -a; popd
